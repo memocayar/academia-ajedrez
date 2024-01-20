4719f46b92f5b652d609cccc3f73f574455a24d6
Elías Mocayar <eliasmocayar@gmail.com>
2024-01-20 16:16:58 -0300

    2do avance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {Banner} from './componentes/Banner'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Banner } from "./componentes/Banner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <>
-    {Banner()}
-  </>
-);
+root.render(<>{Banner()}</>);
