20a2f7c0c1f3abe03ffb249dd799a40eb2dde86f
Elías Mocayar <eliasmocayar@gmail.com>
2024-01-24 00:36:40 -0300

    primer avance de clase diagnostico

---
+++ src/componentes/Clase Diagnostico/clasediagnostico.jsx
@@ -0,0 +1,12 @@
+import "./clasediagnostico.css"
+export function clasediagnostico() {
+    return (
+    <div id ="clase">
+        <div id="textosclase">
+            <h1 id="tituloclase">Clase Diagnóstica Gratuita</h1>
+            <p id="parrafoclase">Descubre tu nivel y diseña junto a nosotros tu ruta de éxito esta primera sesión evaluativa 100% gratuita y sin compromiso.</p>
+        </div>
+        <button id="botonclase">Agendar Ahora</button>
+    </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Banner } from "./componentes/Banner";
+import { Banner } from "./componentes/Banner/Banner";
+import { clasediagnostico } from "./componentes/Clase Diagnostico/clasediagnostico";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 <>
-    {Banner()}
+    {clasediagnostico()}
 
 </>);
