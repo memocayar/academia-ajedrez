c32f11a3977f7840e8b62bad0257996ce70f408e
memocayar <mariaemiliamocayar@gmail.com>
2024-03-31 02:20:44 -0300

    testimonios final

--- src/componentes/testimonios/Testimonios.jsx
+++ src/componentes/testimonios/Testimonios.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./testimonios.css";
 import Card from "./CardTestimonio";
+import { Carousel } from 'react-bootstrap';
+
 
 export default function Testimonios() {
+  const testimonios = [
+    { author: "Card title 11", text: "Testimonio 1" },
+    { author: "Card title 21", text: "Testimonio 2" },
+    { author: "Card title 2.51", text: "Testimonio 3" },
+    {
+      author: "Card title 2.51",
+      text: "Me gusta mucho su forma de trabajar con los alumnos. Porque se enfocan en mejorar los errores que cometes en partidas y perfeccionar tus puntos fuertes.",
+    },
+    { author: "Card title 11", text: "Testimonio 5" },
+    { author: "Card title 21", text: "Testimonio 6" },
+    // Agrega más testimonios aquí
+  ];
+  const testimoniosPorSlide = window.innerWidth >= 992 ? 3 : window.innerWidth >= 768 ? 2 : 1;
+
   return (
     <div className="container testimonios-container">
       <h2 className="titulos-secciones">Testimonios </h2>
@@ -10,77 +26,23 @@ export default function Testimonios() {
         Algunas experiencias de los alumnos de HerMar
       </p>
 
-      <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active card-container">
-            <div className="cards-container">
-              <Card
-                author="Card title 11"
-                text="Me gusta mucho su forma de trabajar con los alumnos. Porque se enfocan en mejorar
-                los errores que cometes en partidas y perfeccionar tus puntos fuertes."
-              />
-              <Card
-                author="Card title 21"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-              <Card
-                author="Card title 2.51"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
+      <Carousel className="cards-container">
+        {testimonios.reduce((acc, testimonio, index) => {
+          if (index % testimoniosPorSlide === 0) acc.push([]);
+          acc[acc.length - 1].push(testimonio);
+          return acc;
+        }, []).map((group, index) => (
+          <Carousel.Item className="carousel-item" key={index}>
+            <div className="row cards-container mx-auto" style={{ maxWidth: 'calc(100% - 10rem)' }}>
+              {group.map((testimonio, index) => (
+                <div className={`col-${12 / testimoniosPorSlide} d-flex justify-content-center`} key={index}>
+                  <Card author={testimonio.author} text={testimonio.text} />
+                </div>
+              ))}
             </div>
-          </div>
-          <div class="carousel-item card-container">
-            <div className="cards-container">
-              <Card
-                author="Card title 11"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-              <Card
-                author="Card title 21"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-              <Card
-                author="Card title 2.51"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-            </div>
-          </div>
-          <div class="carousel-item card-container">
-            <div className="cards-container">
-              <Card
-                author="Card title 11"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-              <Card
-                author="Card title 21"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-              <Card
-                author="Card title 2.51"
-                text="Some quick example text to build on the card title and make up the bulk of the card's content."
-              />
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev carrousel-button"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next carrousel-button"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
