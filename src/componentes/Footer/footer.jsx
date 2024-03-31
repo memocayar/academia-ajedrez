d20298b74618ec16eb10241399d38b93d01a636d
Elías Mocayar <eliasmocayar@gmail.com>
2024-03-31 20:55:35 -0300

    footer terminado

--- src/componentes/Footer/footer.jsx
+++ src/componentes/Footer/footer.jsx
@@ -1,19 +1,34 @@
-import "./footer.css"
-import logoFacebook from "./logoFacebook.png"
-import logoInstagram from "./logoInstagram.png"
+import "./footer.css";
+import logoFacebook from "./logoFacebook.png";
+import logoInstagram from "./logoInstagram.png";
 
 export default function footer() {
-    return (
-        <div id="contenedorFooter">
-            <p id="creditos">© Todos los derechos reservados | María Emilia Mocayar, Elías Mocayar</p>
+  return (
+    <div id="contenedorFooter">
+      <p id="creditos">
+        © Todos los derechos reservados | María Emilia Mocayar, Elías Mocayar
+      </p>
 
-            <a href="https://www.instagram.com/ajedrezhermar" target="_blank" rel="noopener noreferrer">
-                <img src={logoInstagram} id="logoInstagram" alt="Instagram"/>
-            </a>
-            
-            <a href="https://www.facebook.com/ajedrezhermar" target="_blank" rel="noopener noreferrer">
-                <img src={logoFacebook} id="logoFacebook" alt="Facebook"/>
-            </a>
-        </div>
-    )
+      <ul id="listaRedes">
+        <li>
+          <a
+            href="https://www.instagram.com/ajedrezhermar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logoInstagram} id="logoInstagram" alt="Instagram" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/ajedrezhermar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logoFacebook} id="logoFacebook" alt="Facebook" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
