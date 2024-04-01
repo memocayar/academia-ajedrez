b5b8b04bd5a459f4113e3fb31682ec0420b92ede
memocayar <mariaemiliamocayar@gmail.com>
2024-04-01 17:39:14 -0300

    merging definitivo

--- src/componentes/Footer/footer.jsx
+++ src/componentes/Footer/footer.jsx
@@ -2,19 +2,12 @@ import "./footer.css";
 import logoFacebook from "./logoFacebook.png";
 import logoInstagram from "./logoInstagram.png";
 
-<<<<<<< HEAD
-export default function Footer() {
-    return (
-        <div id="contenedorFooter">
-            <p id="creditos">© Todos los derechos reservados | María Emilia Mocayar, Elías Mocayar</p>
-=======
 export default function footer() {
   return (
     <div id="contenedorFooter">
       <p id="creditos">
         © Todos los derechos reservados | María Emilia Mocayar, Elías Mocayar
       </p>
->>>>>>> d20298b74618ec16eb10241399d38b93d01a636d
 
       <ul id="listaRedes">
         <li>
--- src/componentes/faq/FAQ.jsx
+++ src/componentes/faq/FAQ.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import AcordionItem from "./AcordionItem";
 
+const faqData = [
+  {
+    id: "flush-collapseOne",
+    title: "¿Puedo acceder a las clases en cualquier momento, o hay un horario específico?",
+    content: "Las clases en vivo son en horarios a coordinar entre la disponibilidad del alumno y la academia. Adicionalmente, todos los meses tendrás disponible también una clase extra pregrabada en vídeo (sobre un tema específico, que irán variando) para que puedas verla en cualquier momento.",
+  },
+  {
+    id: "flush-collapseTwo",
+    title: "¿Necesito experiencia previa para unirme a la Academia?",
+    content: "No necesitas experiencia previa para unirte a la Academia. Nuestras clases están diseñadas para todos los niveles, desde principiantes hasta jugadores avanzados. Luego de un primer acercamiento a modo de diagnóstico, elaboraremos una planificación personalizada para cada estudiante, de modo tal que cada uno pueda entrenar y mejorar a su ritmo, y con nuestro apoyo constante.",
+  },
+  {
+    id: "flush-collapseThree",
+    title: "¿Qué requerimientos tecnológicos son necesarios para tomar clases?",
+    content: "Para acceder a las clases necesitarás una conexión a internet estable, y un ordenador. Recomendamos que sea una PC de escritorio o Notebook, así podrás acceder al tablero en mayor tamaño. Además, un micrófono para la llamada de voz. Puedes conectarte a la llamada a través de un teléfono celular, por ejemplo, y a la plataforma de ajedrez a través del computador. La cámara web no es obligatoria, aunque recomendamos mantenerla encendida para hacer la clase más amena.",
+  },
+];
+
 export default function FAQ() {
-  const faqData = [
-    {
-      id: "flush-collapseOne",
-      title: "Pregunta 1",
-      content: "Contenido de la pregunta 1",
-    },
-    {
-      id: "flush-collapseTwo",
-      title: "Pregunta 2",
-      content: "Contenido de la pregunta 2",
-    },
-    {
-      id: "flush-collapseThree",
-      title: "Pregunta 3",
-      content: "Contenido de la pregunta 3",
-    },
-  ];
   return (
     <div className="container">
       <h2 className="titulos-secciones">Preguntas Frecuentes</h2>
       <p>Todo lo que necesitas saber acerca de nuestra academia</p>
 
       <div className="accordion accordion-flush" id="accordionFlushExample">
-<<<<<<< HEAD
         {faqData.map((item, index) => (
           <AcordionItem
             key={index}
@@ -34,35 +34,6 @@ export default function FAQ() {
             content={item.content}
           />
         ))}
-=======
-        <AcordionItem
-          id="flush-collapseOne"
-          title="¿Puedo acceder a las clases en cualquier momento, o hay un horario específico?"
-          content="Las clases en vivo son en horarios a coordinar entre la disponibilidad del alumno y la 
-          academia. Adicionalmente, todos los meses tendrás disponible también una clase extra 
-          pregrabada en vídeo (sobre un tema específico, que irán variando) para que puedas verla 
-          en cualquier momento. "
-        />
-        <AcordionItem
-          id="flush-collapseTwo"
-          title="¿Necesito experiencia previa para unirme a la Academia?"
-          content="No necesitas experiencia previa para unirte a la Academia. Nuestras clases están 
-          diseñadas para todos los niveles, desde principiantes hasta jugadores avanzados. Luego 
-          de un primer acercamiento a modo de diagnóstico, elaboraremos una planificación 
-          personalizada para cada estudiante, de modo tal que cada uno pueda entrenar y mejorar 
-          a su ritmo, y con nuestro apoyo constante."
-        />
-        <AcordionItem
-          id="flush-collapseThree"
-          title="¿Qué requerimientos tecnológicos son necesarios para tomar clases?"
-          content="Para acceder a las clases necesitarás una conexión a internet estable, y un ordenador. 
-          Recomendamos que sea una PC de escritorio o Notebook, así podrás acceder al tablero 
-          en mayor tamaño. Además, un micrófono para la llamada de voz. Puedes conectarte a 
-          la llamada a través de un teléfono celular, por ejemplo, y a la plataforma de ajedrez a 
-          través del computador. La cámara web no es obligatoria, aunque recomendamos 
-          mantenerla encendida para hacer la clase más amena."
-        />
->>>>>>> d20298b74618ec16eb10241399d38b93d01a636d
       </div>
 
       <button type="button" class="btn btn-outline-secondary">
